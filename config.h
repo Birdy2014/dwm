100b88561c53d70cd806b7d111adce1aab17d232
Moritz Vogel <moritzv7@googlemail.com>
2020-06-08 18:35:45 +0200

    add window swallowing

--- config.h
+++ config.h
@@ -5,6 +5,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 0;        /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
@@ -31,12 +32,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class         instance title       tags mask     isfloating   monitor */
-    { "firefox",     NULL,    NULL,       1 << 1,       0,           -1 },
-    { "Steam",       NULL,    NULL,       1 << 4,       0,           0 },
-    { "discord",     NULL,    NULL,       1 << 0,       0,           1 },
-    { "code-oss",    NULL,    NULL,       1 << 2,       0,           0 },
-    { "Thunderbird", NULL,    NULL,       1 << 5,       0,           0 },
+	/* class         instance title           tags mask     isfloating    isterminal noswallow   monitor */
+    { "firefox",     NULL,    NULL,           1 << 1,       0,            0,         0,          -1 },
+    { "Steam",       NULL,    NULL,           1 << 4,       0,            0,         0,          0 },
+    { "discord",     NULL,    NULL,           1 << 0,       0,            0,         0,          1 },
+    { "code-oss",    NULL,    NULL,           1 << 2,       0,            0,         0,          0 },
+    { "Thunderbird", NULL,    NULL,           1 << 5,       0,            0,         0,          0 },
+    { "st-256color", NULL,    NULL,           NULL,         0,            1,         0,          -1 },
+    { NULL,          NULL,    "Event Tester", NULL,         0,            0,         1,          -1 },
 };
 
 /* layout(s) */
