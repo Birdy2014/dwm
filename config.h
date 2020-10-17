b493ff9b71d9ddf1a9bd94b7d03c3402901f53de
Moritz Vogel <moritzv7@googlemail.com>
2020-10-17 22:35:41 +0200

    set snap to 5

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
-static const unsigned int snap      = 0;        /* snap pixel */
+static const unsigned int snap      = 5;        /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
